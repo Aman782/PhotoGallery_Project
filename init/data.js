54a68da7a5e64a677639f097b54c260f49311cc6
Aman Pandey <amanpandey45692@gmail.com>
2024-06-29 21:10:15 +0530

    Third commit

---
+++ init/data.js
@@ -0,0 +1,125 @@
+const sampleData = [
+    {
+        title: "tommy one",
+        description: "A cute and innocent puppy!",
+        image: "m1.jpg",
+        location: "street view, los angiles",
+        country: "United States America",
+        owner: [],
+    },
+
+    {
+        title: "beautiful evening",
+        description: "Moments under skys!",
+        image: "m3.jpg",
+        location: "Goa beach",
+        country: "India",
+        owner: [],
+    },
+
+    {
+        title: "Shop Near my Home",
+        description: "Essential shops for locals",
+        image: "m5.jpg",
+        location: "m-36 Hakway street",
+        country: "Argentina",
+        owner: [],
+    },
+
+    {
+        title: "Old Home",
+        description: "It my childhood home",
+        image: "m6.jpg",
+        location: "l-458 new kilester house",
+        country: "United States America",
+        owner: [],
+    },
+
+
+    {
+        title: "Water Canal",
+        description: "A Water canal into the farm land",
+        image: "m7.jpg",
+        location: "Kevin's farm",
+        country: "Germany",
+        owner: [],
+    },
+
+    {
+        title: "Yellow flower",
+        description: "Beautiful flower in my garden",
+        image: "m8.jpg",
+        location: "my cherious garden",
+        country: "Canada, North America",
+        owner: [],
+    },
+
+
+    {
+        title: "White flower",
+        description: "Beautiful flower in my office terrace",
+        image: "m9.jpg",
+        location: "Tower 365, IBM",
+        country: "Australia",
+        owner: [],
+    },
+
+    {
+        title: "Jasmine flower",
+        description: "Garden flower",
+        image: "m10.jpg",
+        location: "New York",
+        country: "United States America",
+        owner: [],
+    },
+
+
+    {
+        title: "Sunflower",
+        description: "Beautiful sunflower",
+        image: "m11.jpg",
+        location: "wexico garden",
+        country: "New Zealand",
+        owner: [],
+    },
+
+
+    {
+        title: "Tree Garden",
+        description: "Beautiful Picture by ",
+        image: "m12.jpg",
+        location: "barbados",
+        country: "West Indies",
+        owner: [],
+    },
+
+
+    {
+        title: "Leafs",
+        description: "Smart & legend pic",
+        image: "m13.jpg",
+        location: "Guyana",
+        country: "West Indies",
+        owner: [],
+    },
+
+    {
+        title: "Sky is the limit",
+        description: "Beautiful weather",
+        image: "m15.jpg",
+        location: "St. Lucia",
+        country: "United States of America",
+        owner: [],
+    },
+
+
+    {
+        title: "Yumm Cake",
+        description: "Tasty cake for my friend",
+        image: "m16.jpg",
+        location: "My home",
+        country: "Srilanka",
+        owner: [],
+    },
+
+]
