525f2eb232b6ed12c723c55b2e4793ea4a4461ae
Aman Pandey <amanpandey45692@gmail.com>
2024-08-01 19:19:26 +0530

    01-08-24

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./Utils/ExpressError');
 const Review = require('./models/reviews.js');
-const listingsRoutes = require('./router/listing')
+const listingsRoutes = require('./router/listing');
 const reviewRoutes = require('./router/review');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+const User = require('./models/user.js');
 
 
 const url = "mongodb://127.0.0.1:27017/photosgallery";
@@ -27,23 +32,52 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
+const sessionOptions = {
+    secret: "mysecretcode",
+    resave: false,
+    saveUninitialized: true,
+};
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()))
+
+// In passport method the hashing function used is "pbkdf2" algorithm
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    next();
+});
+
 app.listen(4000, () => {
     console.log("listening at port 4000");
 });
 
+app.get('/register', async (req, res)=>{
+    let demoUser = new User({
+        email:"demouser@gmail.com",
+        username: "developer",
+    });
+
+    let regDemoUser = await User.register(demoUser, "hellodemoUser");
+    res.send(regDemoUser);
+})
 
 // Root route
 app.get("/", (req, res) => {
     res.send("HI, I am root");
 });
 
-
 // Home Route
 app.get('/home', async (req, res) => {
     const allListings = await Listing.find({});
     res.render("./listings/home.ejs", { allListings });
 });
-
 
 // Show route
 app.get('/home/:id', async (req, res) => {
@@ -58,15 +92,14 @@ app.get('/home/:id', async (req, res) => {
     }
 });
 
-app.use('/listing', listingsRoutes);
+app.use('/listing', listingsRoutes);  // Ensure this matches the form action
 app.use('/listing/:id/reviews', reviewRoutes);
 
-
-app.all("*", (req, res, next)=>{
+app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
 });
 
-app.use((err, req, res, next)=>{
-    let {statusCode=500, message="Something went wrong"} = err;
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong" } = err;
     res.status(statusCode).send(message);
-})
+});
--- router/listing.js
+++ router/listing.js
@@ -1,67 +1,58 @@
 const express = require('express');
 const Listing = require('../models/Listing');
 const ExpressError = require('../Utils/ExpressError');
-const router = express.Router({mergeParams:true});
+const router = express.Router({ mergeParams: true });
 
-
-// Show Route
-router.get('/listing/new', (req, res) => {
-    res.render('../listings/new.ejs');
+// Route to show the new listing form
+router.get('/new', (req, res) => {  // Changed the path here
+    res.render('listings/new.ejs');  // Make sure the path is correct
 });
 
-
 // Create Route
-router.post('/listing/new', async (req, res, next) => {
+router.post('/new', async (req, res, next) => {
     try {
         const newListing = new Listing(req.body.listing);
         await newListing.save();
-        console.log(req.body)
+        req.flash("success", "Post Uploaded Successfully!");
         res.redirect('/home');
     } catch (error) {
-        // console.error(error);
-        // res.status(400).send("Error creating new listing");
-        next(err);
+        next(error);
     }
 });
 
 // Edit Route
-router.get('/listing/edit/:id', async (req, res, next) => {
-    try{
+router.get('/edit/:id', async (req, res, next) => {
+    try {
         let { id } = req.params;
         let listing = await Listing.findById(id);
-        console.log(id);
-        console.log(listing);
-        res.render('../listings/update.ejs', { listing });
-    }catch(err){
+        res.render('listings/update.ejs', { listing });  // Make sure the path is correct
+    } catch (err) {
         next(err);
     }
-    
 });
-
 
 // Update Route
-router.put('/listing/edit/:id', async (req, res) => {
-    try{
+router.put('/edit/:id', async (req, res) => {
+    try {
         let { id } = req.params;
         await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+        req.flash("success", "Post Updated Successfully!");
         res.redirect('/home');
-    }catch(e){
-        console.log(e)
+    } catch (e) {
+        console.log(e);
     }
-    
 });
 
-
 // Delete Route
-router.delete('/listing/delete/:id', async (req, res, next) => {
-    try{
+router.delete('/delete/:id', async (req, res, next) => {
+    try {
         let { id } = req.params;
         await Listing.findByIdAndDelete(id);
+        req.flash("success", "Post Deleted Successfully!");
         res.redirect('/home');
-    }catch(err){
+    } catch (err) {
         next(err);
     }
-    
 });
 
 module.exports = router;
